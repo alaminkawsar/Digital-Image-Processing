1d44ceb6aa31203a92ada4f7c57317147295b9d3
alaminkawsar <alaminkawsar8643@gmail.com>
2022-12-03 15:42:24 +0600

    erosion dialtion edited

--- Assignment/1810576130_Assignment9/custom_erosion_dilation.py
+++ Assignment/1810576130_Assignment9/custom_erosion_dilation.py
@@ -11,16 +11,15 @@ def countOne(kernel):
                 cnt += 1
     return cnt
 
-def padding(img:np.array)->np.array:
+def padding(img):
     w, h = img.shape
     new_img = np.zeros(shape=(w+2,h+2))
     w, h = new_img.shape
     new_img[1:w-1,1:h-1] = img
-    new_img.astype(int)
     return new_img
 
-def erosion(img: int, kernel: int)->np.array:
-    img = padding(np.array(img))
+def erosion(img, kernel):
+    img = padding(img)
     _, k = kernel.shape
     w, h = img.shape
     new_w, new_h = w-k+1, h-k+1
@@ -41,36 +40,33 @@ def erosion(img: int, kernel: int)->np.array:
                 conv_img[i,j]=0;
     return conv_img
 
-def dilation(img: int, kernel: int)->np.array:
-    img = padding(np.array(img))
+def dilation(img, kernel):
+    img = padding(img)
     _, k = kernel.shape
     w, h = img.shape
     new_w, new_h = w-k+1, h-k+1
     conv_img = np.zeros(shape=(new_w,new_h))
-    
-    #number of one count
-    cnt = countOne(kernel=kernel)
-    #print(cnt)
+
     for i in range(new_w):
         for j in range(new_h):
             mat = img[i:i+k,j:j+k]
             flag = countOne(np.multiply(kernel,mat))
             
             if flag>0:
-                conv_img[i,j]=1
+                conv_img[i][j]=1
             else:
-                conv_img[i,j]=0;
+                conv_img[i][j]=0;
     return conv_img
 
-# start from here
 
-img_path = '/home/kawsar/Desktop/Class_Resource/4th year 1st semester/4181- Digital Image Processing/ImageProcessingLab/Assignment/1810576130_Assignment9/tree.jpg'
+# start from here
+img_path = '/home/kawsar/Desktop/Class_Resource/4th year 1st semester/4181- Digital Image Processing/ImageProcessingLab/Assignment/1810576130_Assignment9/A.png'
 img = cv2.imread(img_path,0)
 
 #img = cv2.resize(img,(300,300))
 _, img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
 
-x, y, pos = 1, 3, 1
+x, y, pos = 2, 3, 1
 List = []
 List.append([img,'Main Image'])
 
@@ -85,8 +81,12 @@ List.append([img_dilation,'Built in Dilation Image'])
 custom_dilation = dilation(img, kernel1)
 List.append([custom_dilation,'Custom Dialtion Image'])
 
-#Built in opening image
-
+#Built in erosion
+img_erosion = cv2.erode(img,kernel1, iterations=1)
+List.append([img_erosion,'Built in erosion Image'])
+#custom dilation
+custom_erosion = erosion(img, kernel1)
+List.append([custom_erosion,'Custom erosion Image'])
 
 
 for im, title in List:
